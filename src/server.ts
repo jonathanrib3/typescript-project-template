b41520db3652ee73a07174545191bcedd767cbf9
jonathanrib3 <jonathanrib.3@gmail.com>
2022-01-14 11:05:38 -0300

    add: eslint and jest configs

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express, { json } from "express";
 
-
-const app = express(); 
+const app = express();
 
 app.use(json());
 
-app.get("/", (request, response) => {
-  return response.status(200).send({message: "Welcome!!"});
-})
+app.get("/", (request, response) =>
+  response.status(200).send({ message: "Welcome!!" })
+);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost/3000");
